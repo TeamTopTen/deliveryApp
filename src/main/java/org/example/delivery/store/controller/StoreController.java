a795a859d7284fbcd4954c48765c22774d7da251
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-01-08 15:00:44 +0900

    ✨feat 🧹 chore : 1. change StoreCreateRequest to StoreRequest 2. add update logic

--- src/main/java/org/example/delivery/store/controller/StoreController.java
+++ src/main/java/org/example/delivery/store/controller/StoreController.java
@@ -2,12 +2,15 @@ package org.example.delivery.store.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.delivery.store.model.request.StoreCreateRequest;
+import org.example.delivery.store.model.request.StoreRequest;
 import org.example.delivery.store.model.response.StoreResponse;
 import org.example.delivery.store.service.StoreService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +24,20 @@ public class StoreController {
   private final StoreService storeService;
 
   @PostMapping("/owners")
-  public ResponseEntity<StoreResponse> createStore(@RequestBody StoreCreateRequest request) {
+  public ResponseEntity<StoreResponse> createStore(@RequestBody StoreRequest request) {
 
     StoreResponse response = storeService.createStore(request);
 
     return new ResponseEntity<>(response, HttpStatus.CREATED);
+  }
+
+  @PutMapping("{store_id}/owner")
+  public ResponseEntity<StoreResponse> updateStore(
+      @PathVariable(name = "store_id") Long storeId, @RequestBody StoreRequest request) {
+
+    StoreResponse response = storeService.updateStore(storeId, request);
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
 }
--- src/main/java/org/example/delivery/store/service/StoreService.java
+++ src/main/java/org/example/delivery/store/service/StoreService.java
@@ -7,7 +7,7 @@ import org.example.delivery.common.domain.Store;
 import org.example.delivery.common.domain.User;
 import org.example.delivery.common.exception.ErrorCode;
 import org.example.delivery.common.exception.base.NotFoundException;
-import org.example.delivery.store.model.request.StoreCreateRequest;
+import org.example.delivery.store.model.request.StoreRequest;
 import org.example.delivery.store.model.response.StoreResponse;
 import org.example.delivery.store.repository.StoreRepository;
 import org.example.delivery.user.repository.UserRepository;
@@ -22,15 +22,26 @@ public class StoreService {
   private UserRepository userRepository;
   private final HttpSession session;
 
-  public StoreResponse createStore(StoreCreateRequest request) {
+  public StoreResponse createStore(StoreRequest request) {
 
-    Long userId = (Long) session.getAttribute("logined_user");
+    Long userId = (Long) session.getAttribute("logged_user");
     User foundUser = userRepository.findById(userId)
         .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
 
-    Store store = Store.registerStore(foundUser, request);
+    Store store = Store.makeWith(foundUser, request);
     storeRepository.save(store);
 
     return new StoreResponse("가게가 정상적으로 등록 완료되었습니다.");
+  }
+
+
+  public StoreResponse updateStore(Long storeId, StoreRequest request) {
+
+    Store foundStore = storeRepository.findById(storeId)
+        .orElseThrow(() -> new NotFoundException(ErrorCode.STORE_NOT_FOUND));
+
+    storeRepository.save(foundStore.updateWith(request));
+
+    return new StoreResponse("수정이 정상적으로 완료되었습니다.");
   }
 }

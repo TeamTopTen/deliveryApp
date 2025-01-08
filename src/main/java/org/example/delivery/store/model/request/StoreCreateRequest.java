50fc50b8fade81fac15d3b3db0b3f89371aef869
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-01-08 15:01:51 +0900

    🚛 update : move content of StoreCreateRequest to StoreRequest

--- src/main/java/org/example/delivery/store/model/request/StoreCreateRequest.java
+++ src/main/java/org/example/delivery/store/model/request/StoreCreateRequest.java
@@ -5,29 +5,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import java.sql.Time;
 
-public record StoreCreateRequest (
+public class StoreCreateRequest {
 
-  @NotBlank
-  String name,
-
-  @Pattern(
-      regexp = "^(01[0-9])-(\\d{3,4})-(\\d{4})$",
-      message = "올바른 전화번호 양식이 필요합니다."
-  )
-  String storeNumber,
-
-  @NotBlank
-  String storeAddress,
-
-  @Pattern(
-      regexp = "^\\d{3}-\\d{2}-\\d{5}$",
-      message = "올바른 사업자번호 양식을 입력해주세요."
-  )
-  String registrationNumber,
-
-  @NotNull
-  Time openingTime,
-
-  @NotNull
-  Time closingTime
-) {}
+}
--- src/main/java/org/example/delivery/store/model/request/StoreRequest.java
+++ src/main/java/org/example/delivery/store/model/request/StoreRequest.java
@@ -1,5 +1,36 @@
 package org.example.delivery.store.model.request;
 
-public class StoreRequest {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import java.sql.Time;
 
-}
+public record StoreRequest (
+
+@NotBlank
+String name,
+
+@Pattern(
+    regexp = "^(01[0-9])-(\\d{3,4})-(\\d{4})$",
+    message = "올바른 전화번호 양식이 필요합니다."
+)
+String storeNumber,
+
+@NotBlank
+String storeAddress,
+
+@Pattern(
+    regexp = "^\\d{3}-\\d{2}-\\d{5}$",
+    message = "올바른 사업자번호 양식을 입력해주세요."
+)
+String registrationNumber,
+
+@NotBlank
+Integer minOrderPrice,
+
+@NotNull
+Time openingTime,
+
+@NotNull
+Time closingTime
+) {}

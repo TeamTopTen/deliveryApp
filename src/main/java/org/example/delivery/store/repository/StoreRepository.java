45ed06d6aa4b27b4a7dc995c291f85c0dd1132e7
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-01-08 15:02:17 +0900

    🧹 chore : change ID type

--- src/main/java/org/example/delivery/store/repository/StoreRepository.java
+++ src/main/java/org/example/delivery/store/repository/StoreRepository.java
@@ -3,6 +3,6 @@ package org.example.delivery.store.repository;
 import org.example.delivery.common.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreRepository extends JpaRepository<Store, Integer> {
+public interface StoreRepository extends JpaRepository<Store, Long> {
 
 }

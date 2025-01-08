429d07b93d62b23f56a81d36855ac5bb3cefa9b0
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-01-08 16:41:19 +0900

    🧹 chore : fix Code Convention

--- src/main/java/org/example/delivery/store/model/dto/request/StoreRequest.java
+++ src/main/java/org/example/delivery/store/model/dto/request/StoreRequest.java
@@ -5,32 +5,34 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import java.sql.Time;
 
-public record StoreRequest (
+public record StoreRequest(
 
-@NotBlank
-String name,
+    @NotBlank
+    String name,
 
-@Pattern(
-    regexp = "^(01[0-9])-(\\d{3,4})-(\\d{4})$",
-    message = "올바른 전화번호 양식이 필요합니다."
-)
-String storeNumber,
+    @Pattern(
+        regexp = "^(01[0-9])-(\\d{3,4})-(\\d{4})$",
+        message = "올바른 전화번호 양식이 필요합니다."
+    )
+    String storeNumber,
 
-@NotBlank
-String storeAddress,
+    @NotBlank
+    String storeAddress,
 
-@Pattern(
-    regexp = "^\\d{3}-\\d{2}-\\d{5}$",
-    message = "올바른 사업자번호 양식을 입력해주세요."
-)
-String registrationNumber,
+    @Pattern(
+        regexp = "^\\d{3}-\\d{2}-\\d{5}$",
+        message = "올바른 사업자번호 양식을 입력해주세요."
+    )
+    String registrationNumber,
 
-@NotBlank
-Integer minOrderPrice,
+    @NotBlank
+    Integer minOrderPrice,
 
-@NotNull
-Time openingTime,
+    @NotNull
+    Time openingTime,
 
-@NotNull
-Time closingTime
-) {}
+    @NotNull
+    Time closingTime
+) {
+
+}

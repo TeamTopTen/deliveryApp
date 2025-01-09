8597d85370b382ea5e3ab4ce94bcc2b05c1d257f
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-01-09 15:38:37 +0900

    🎨 perf: change field 'List<Menu>' to 'List<MenuResponse>'

--- src/main/java/org/example/delivery/store/model/dto/response/GetStoreByIdResponse.java
+++ src/main/java/org/example/delivery/store/model/dto/response/GetStoreByIdResponse.java
@@ -2,8 +2,8 @@ package org.example.delivery.store.model.dto.response;
 
 import java.sql.Time;
 import java.util.List;
-import org.example.delivery.common.domain.Menu;
 import org.example.delivery.common.domain.Store;
+import org.example.delivery.menu.model.response.MenuResponse;
 
 public record GetStoreByIdResponse(
 
@@ -13,10 +13,10 @@ public record GetStoreByIdResponse(
     Integer minOrderPrice,
     Time openingTime,
     Time closingTime,
-    List<Menu> menus
+    List<MenuResponse> menus
 ) {
 
-  public static GetStoreByIdResponse with(Store store, List<Menu> menus) {
+  public static GetStoreByIdResponse with(Store store, List<MenuResponse> menus) {
 
     return new GetStoreByIdResponse(store.getName(), store.getStoreNumber(),
         store.getStoreAddress(), store.getMinOrderPrice(), store.getOpeningTime(),

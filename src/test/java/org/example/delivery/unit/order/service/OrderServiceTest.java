a18cb272815def2d94cba08a63d0b2f0826e16c1
TE <gimt75257@gmail.com>
2025-01-10 09:34:44 +0900

    :sparkles:TestCode(OrderService_PostAPI_failCASE_3)

--- src/test/java/org/example/delivery/unit/order/service/OrderServiceTest.java
+++ src/test/java/org/example/delivery/unit/order/service/OrderServiceTest.java
@@ -150,7 +150,7 @@ public class OrderServiceTest {
   }
 
   @Test
-  public void 주문_생성_실패_CASE_2_userId가_다를때_테스트() {
+  public void 주문_생성_실패_CASE_2_user가_없을때_테스트() {
     //given
     String email = "Test1234@test.com";
     UserRole userRole = UserRole.USER;
@@ -201,6 +201,56 @@ public class OrderServiceTest {
 
   }
 
+  @Test
+  public void 주문_생성_실패_CASE_3_store가_없을때_테스트() {
+    //given
+    String email = "Test1234@test.com";
+    UserRole userRole = UserRole.USER;
+    String userName = "test";
+    String password = "Test1234!@#$";
+    String phoneNumber = "0101111111";
+    String userAddress = "testaddress";
 
+    String storeName = "맛나식당";
+    String storeNumber = "01011111111";
+    String storeAddress = "한국 어딘가";
+    String registrationNumber = "111-11-11111";
+    Integer minOrederPrice = 1000;
+    String  stringOpeningTime = "7:00:00";
+    Time openingTime = Time.valueOf(stringOpeningTime);
+    String  stringClosingTime = "22:00:00";
+    Time closingTime = Time.valueOf(stringClosingTime);
+    String  stringCurrentTime = "12:11:10";
+    Time currentTime = Time.valueOf(stringCurrentTime);
+
+    String menuName = "맛있는 음식";
+    Integer menuPrice = 2000;
+
+    OrderStatus orderStatus = OrderStatus.of("ORDERED");
+
+    Long userId = 1L;
+    Long storeId = 1L;
+    Long menuId = 1L;
+
+    AuthUser authUser = new AuthUser(userId, email, userRole);
+    User user = new User(email,password,userName,phoneNumber,userAddress,userRole);
+    Store store = new Store(user,storeName,storeNumber,storeAddress,registrationNumber,minOrederPrice,openingTime,closingTime);
+    Menu menu = Menu.menuCreate(menuName, menuPrice, store, user);
+    Order order = new Order(user, store, menu, orderStatus);
+
+    when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+    when(storeRepository.findById(storeId)).thenReturn(Optional.ofNullable(null));
+
+    //when
+    BusinessException businessException = Assertions.assertThrows(BusinessException.class, () -> {
+      orderService.createOrder(authUser, storeId, menuId);
+    });
+
+    //then
+    verify(storeRepository,times(ONE_TIME)).findById(storeId);
+    verify(menuRepository,never()).findById(menuId);
+    org.assertj.core.api.Assertions.assertThat(businessException.getMessage()).isEqualTo(ErrorCode.STORE_NOT_FOUND.getMessage());
+
+  }
 
 }

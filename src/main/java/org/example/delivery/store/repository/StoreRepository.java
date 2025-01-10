6b0f02e68e88ef071887edc282c07f6d7497491b
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-01-10 13:43:01 +0900

    ✨feat : add and apply findStoreByIdAndIsDeletedFalse

--- src/main/java/org/example/delivery/store/repository/StoreRepository.java
+++ src/main/java/org/example/delivery/store/repository/StoreRepository.java
@@ -1,6 +1,7 @@
 package org.example.delivery.store.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.example.delivery.common.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,6 +11,7 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 
   boolean existsStoreByName(String name);
   boolean existsStoreByStoreAddress(String address);
+  Optional<Store> findStoreByIdAndIsDeletedFalse(long id);
   List<Store> findAllByIsDeletedFalse();
 
 }
